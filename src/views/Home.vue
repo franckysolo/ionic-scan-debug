59fbae245d306d810cb17b9af8e08abbf18c5458
franckysolo <franckysolo@gmail.com>
2021-02-01 15:48:50 +0100

    Initial commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,66 +3,89 @@
     <ion-header :translucent="true">
       <ion-toolbar>
         <ion-title>Blank</ion-title>
+        <ion-back-button slot="end"></ion-back-button>
       </ion-toolbar>
     </ion-header>
-    
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
-    
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-      </div>
+    <ion-content class="ion-padding">
+      <ion-button @click="scan">Scanner un code barre</ion-button>
+      <pre>
+        initial values {{ form }}
+        errors  {{ errors }}
+        form values {{ values }}
+        vinData {{ vinData }}
+      </pre>
     </ion-content>
   </ion-page>
 </template>
 
-<script lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
-import { defineComponent } from 'vue';
-
+<script>
+import {
+  defineComponent
+} from 'vue'
+import {
+  IonHeader,
+  IonTitle,
+  IonButton,
+  IonToolbar,
+  IonPage,
+//  IonInput,
+  IonContent,
+  IonBackButton
+} from '@ionic/vue'
+import { BarcodeScanner } from '@ionic-native/barcode-scanner'
+import { sample } from 'lodash'
 export default defineComponent({
-  name: 'Home',
+  name: 'Scan',
   components: {
-    IonContent,
     IonHeader,
-    IonPage,
     IonTitle,
-    IonToolbar
+    IonButton,
+    IonToolbar,
+  //  IonInput,
+    IonPage,
+    IonBackButton,
+    IonContent,
+  },
+  data () {
+    return {
+      vin: null,
+      vinData: {},
+      form: {
+        vin: ''
+      },
+      schema: {
+        vin: 'required'
+      }
+    }
+  },
+  methods: {
+    async populateVin () {
+      const vins = [
+        '4Y1SL65848Z411439',
+        '1GNALDEK9FZ108495',
+        '2C4RDGBG6ER405565',
+        'WDBNG75J04A417726',
+        '1FAHP3F20CL266328',
+        '5XYZWDLA4EG145416',
+        'JTDKB20U497855273',
+      ]
+      this.form.vin = sample(vins)
+      console.info(this.form.vin)
+
+    },
+    async onSubmit (values) {
+      console.log(values)
+    },
+    async scan () {
+      try {
+        const data = await BarcodeScanner.scan()
+        console.info(`Barcode data: ${data.text}`)
+        this.vinData = data
+        this.form.vin = data.text
+      } catch (e) {
+        console.info(e)
+      }
+    }
   }
-});
+})
 </script>
-
-<style scoped>
-#container {
-  text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-}
-
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
-}
-</style>
